b32f3c17aaab6e223256d6f8b0d766eff4819962
JackSmart1234 <dodoankhac@gmail.com>
2023-05-30 21:08:04 +0700

    finish code and make hashtable

--- hashtable.c
+++ hashtable.c
@@ -5,6 +5,8 @@
 
 struct hashtable {
     // TODO: define hashtable struct to use linkedlist as buckets
+    linkedlist_t **buckets;
+    int length;
 };
 
 /**
@@ -18,18 +20,37 @@ static int hash(int key, int max_range) {
 
 hashtable_t *ht_init(int num_buckets) {
     // TODO: create a new hashtable
+    hashtable_t *table = malloc(sizeof(hashtable_t));
+    table->buckets = malloc(sizeof(linkedlist_t*) * num_buckets);
+    table->length = num_buckets;
+
+    for (int i = 0; i < num_buckets; i++) {
+        table->buckets[i] = ll_init();
+    }
+
+    return table;
 }
 
 void ht_add(hashtable_t *table, int key, int value) {
     // TODO: create a new mapping from key -> value.
     // If the key already exists, replace the value.
+    int index = hash(key, table->length);
+    ll_add(table->buckets[index], key, value);
+
 }
 
 int ht_get(hashtable_t *table, int key) {
     // TODO: retrieve the value mapped to the given key.
     // If it does not exist, return 0
+    int index = hash(key, table->length);
+    return ll_get(table->buckets[index], key);
 }
 
 int ht_size(hashtable_t *table) {
     // TODO: return the number of mappings in this hashtable
+    int number_of_mapping = 0;
+    for (int i = 0; i < table->length; i++) {
+        number_of_mapping += ll_size(table->buckets[i]);
+    }
+    return number_of_mapping;
 }
